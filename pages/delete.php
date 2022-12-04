e1403064cf15da9e69289266d0d93c082a391327
loveagile <happyplandry@gmail.com>
2022-12-04 07:05:22 -0500

    added: delete page

---
+++ pages/delete.php
@@ -0,0 +1,60 @@
+<?php
+require_once "../config/index.php";
+
+if (isset($_POST["id"]) && !empty($_POST["id"])) {
+    $sql = "DELETE FROM models WHERE id = ?";
+
+    if ($stmt = mysqli_prepare($link, $sql)) {
+        mysqli_stmt_bind_param($stmt, "i", $param_id);
+
+        $param_id = trim($_POST["id"]);
+        if (mysqli_stmt_execute($stmt)) {
+            header("location: ../index.php");
+            exit();
+        } else {
+            echo "Please try again later.";
+        }
+    }
+    mysqli_stmt_close($stmt);
+    mysqli_close($link);
+} else {
+    if (empty(trim($_GET["id"]))) {
+        header("location: error.php");
+        exit();
+    }
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Delete Model</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+    <div class="wrapper">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    <h2 class="mt-5 mb-3">Delete Model</h2>
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="alert alert-danger">
+                            <input type="hidden" name="id" value="<?php echo trim($_GET["id"]); ?>" />
+                            <p>Are you sure you want to delete this model?</p>
+                            <p>
+                                <input type="submit" value="Yes" class="btn btn-danger">
+                                <a href="../index.php" class="btn btn-secondary">No</a>
+                            </p>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
